19ef12b36353102f64c5e32f338792fe7eb8f7eb
Loren Norman <lorennorman@gmail.com>
2026-07-15 15:04:09 -0400

    tidy and comment gear icon

--- app/scripts/hacks.js
+++ app/scripts/hacks.js
@@ -350,16 +350,24 @@ Blockly.config.connectingSnapRadius = 64
 // Replace the busy default mutator gear (reads as a snowflake at icon size) with
 // a clean, bold 8-lobe cog.
 ;(() => {
-  const MI = Blockly.icons && Blockly.icons.MutatorIcon
+  const MI = Blockly.icons?.MutatorIcon
   if(!MI) { return }
+
+  // svg datauri of nice gear
   const GEAR = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23ffffff'><path fill-rule='evenodd' d='M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z'/></svg>"
-  const baseInit = MI.prototype.initView
+
+  const baseInitView = MI.prototype.initView
   MI.prototype.initView = function(listener) {
-    baseInit.call(this, listener)
+    baseInitView.call(this, listener)
+
     const root = this.svgRoot
     if(!root) { return }
+
+    // hide the original gear
     const sym = root.querySelector('.blocklyIconSymbol')
     if(sym) { sym.style.display = 'none' }
+
+    // create a new gear and set its href
     const img = Blockly.utils.dom.createSvgElement(
       'image', { 'class': 'io-mutator-gear', width: 16, height: 16, x: 0, y: 0 }, root)
     img.setAttribute('href', GEAR)
